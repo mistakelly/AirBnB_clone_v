00970f6017f80aa70093fc2af5e1834377bb0ebc
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-04-06 12:59:35 +0100

    console update

--- console.py
+++ console.py
@@ -14,8 +14,13 @@ from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 
+print("inside console")
+
 
 class HBNBCommand(cmd.Cmd):
+    """
+        console itself
+    """
 
     Storage = models.storage
     all_obj = Storage.all()
@@ -32,10 +37,16 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_custom_command(self, arg):
+        """
+            ijoiwejfiwejof
+        """
         print('custom')
 
     # dynamically strip quotes of the update method (obj_value)
     def strip_quotes(self, value) -> str:
+        """
+            oijikewf
+        """
         if value is not None:
             if "\'" in value:
                 value = value.strip("'")
@@ -50,10 +61,16 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_EOF(self, arg):
+        """
+            no docstring is found
+        """
         print()
         return True
 
     def do_create(self, arg) -> None:
+        """
+            oijwiejfowe
+        """
         usr_input = arg.split(' ')
 
         if not usr_input[0]:
@@ -64,14 +81,19 @@ class HBNBCommand(cmd.Cmd):
             obj = self.ALL_CLASSES[usr_input[0]]()
             print(obj.id)
             for match in range(1, len(usr_input)):
-                attribute, value = usr_input[match].split('=')
-                if '_' in value:
-                    value = value.replace("_", " ")
-                if hasattr(obj, attribute):
-                    setattr(obj, attribute, value.strip('"'))
+                if "=" in usr_input[match]:
+                    attribute, value = usr_input[match].split('=')
+                    if '_' in value:
+                        value = value.replace("_", " ")
+                    if hasattr(obj, attribute):
+                        if isinstance(getattr(obj, attribute), float):
+                            if '.' not in value:
+                                continue
+                        setattr(obj, attribute, value.strip('"'))
             obj.save()
+            print(obj)
         else:
-            print("** class dosen't exist")
+            print("** class dosen't exist **")
 
     def do_show(self, arg) -> None:
         """
@@ -110,8 +132,10 @@ class HBNBCommand(cmd.Cmd):
             print("Invalid input format "
                   "<show class-name xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx>")
 
-
     def do_delete(self, arg) -> None:
+        """
+            iojoewifjoweifwe
+        """
         usr_input = arg.split()
         all_obj = self.Storage.all()
 
@@ -141,16 +165,19 @@ class HBNBCommand(cmd.Cmd):
             # construct a key using the users info
             key = "{}.{}".format(usr_input[0], usr_input[1])
             if key in all_obj:
-                    del all_obj[key]
-                    self.Storage.save_obj()
-                    return
+                del all_obj[key]
+                self.Storage.save_obj()
+                return
             else:
                 print('** no instance found **')
         else:
             print("Invalid input format "
-                      "<delete class-name xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx>")
+                  "<delete class-name xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx>")
 
     def do_all(self, arg):
+        """
+            oijofew
+        """
         if not arg:
             for k, v in self.all_obj.items():
                 print(v)
@@ -168,25 +195,21 @@ class HBNBCommand(cmd.Cmd):
         else:
             print('** class doesn\'t exist **')
 
-    def calc_updated_at_last(self, model) -> None:
-        model_updated_at = getattr(model, 'updated_at')
-        current_time = datetime.datetime.now()
-        time_difference = current_time - model_updated_at
-        if not time_difference > datetime.timedelta(minutes=1):
-            print("sorry you have to wait for 1minute before you can update account again")
-            return
-
     def match_pattern(self, arg) -> re.Match:
-        # pattern matches string with (single apostrophe <'> or with double <"> or without an apostrophe)
-        pattern =  r"([a-zA-Z_-]+\S+)?\s?([a-zA-Z0-9-]+)?\s?([a-zA-Z_]+)?\s?" \
-                   r"(\'\w+\s?\w+\'|\"\w+\s?\w+|\w+\s?\w+)?"
-#          update BaseModel 7cec092c-83c0-4c56-9415-e60219debab
+        """
+            oijiefwefwe
+        """
+        pattern = r"([a-zA-Z_-]+\S+)?\s?([a-zA-Z0-9-]+)?\s?([a-zA-Z_]+)?\s?" \
+                  r"(\'\w+\s?\w+\'|\"\w+\s?\w+|\w+\s?\w+)?"
         regex = re.compile(pattern)
         match_object = regex.search(arg)
 
         return match_object
 
-    def check_arg(self, key, match_arg_group:tuple) -> None:
+    def check_arg(self, key, match_arg_group: tuple) -> None:
+        """
+            this is the arg for checking for arg in my strings
+        """
         if match_arg_group[0] is None:
             print('** class name missing **')
             return
@@ -199,7 +222,7 @@ class HBNBCommand(cmd.Cmd):
             print('** instance id missing **')
             return
         else:
-            if key not in  self.all_obj:
+            if key not in self.all_obj:
                 print('** no instance found **')
                 return
 
@@ -220,6 +243,7 @@ class HBNBCommand(cmd.Cmd):
         matches = self.match_pattern(arg)
         match_obj = matches.groups()
         key = f"{match_obj[0]}.{match_obj[1]}"
+
         all_obj = self.check_arg(key, match_obj)
 
         if all_obj is not None:
@@ -231,33 +255,35 @@ class HBNBCommand(cmd.Cmd):
 
             if hasattr(model, obj_attri):
                 # only update str, int and float
-                if not isinstance(getattr(model, obj_attri), (str, int, float)):
-                    print(f"Sorry can't update attribute of type {type(getattr(model, obj_attri))}")
+                if not isinstance(getattr(model, obj_attri),
+                                  (str, int, float)):
+                    print(f"Sorry can't update attribute of type"
+                          f" {type(getattr(model, obj_attri))}")
                     return
 
                 if obj_attri not in ['name', 'age', 'email', 'DOB']:
-                    print('sorry you can only update your name, age and email only! ')
+                    print('sorry you can only update your name,'
+                          ' age and email only! ')
                     return
                 else:
                     try:
                         if obj_attri == 'age':
                             if self.strip_quotes(obj_value):
-                                obj_value = self.strip_quotes(obj_value) # strip method.
+                                obj_value = self.strip_quotes(obj_value)
                                 obj_value = int(obj_value)
-                                # self.calc_updated_at_last(model)         # updated_at time differece method
                                 setattr(model, obj_attri, obj_value)
-                                self.Storage.save_obj() # call the save method of the FileStorage to write object back to file.json.
-                                model.save()            # update the updated_at from the BaseModel save method.
+                                self.Storage.save_obj()
+                                model.save()
                                 print(f'({obj_attri}) Updated successfully!')
                                 return
                         else:
                             obj_value = self.strip_quotes(obj_value)
-                            # self.calc_updated_at_last(model)
-                            setattr(model, obj_attri, obj_value) # strip out ("") from the value.
-                            self.Storage.save_obj() # call the save method of the file storage to write back object to file_db
+                            setattr(model, obj_attri, obj_value)
+                            self.Storage.save_obj()
                             model.save()
                     except ValueError:
-                        print(f'Input <{obj_value}> not a valid format for attribute [age],'
+                        print(f'Input <{obj_value}> '
+                              f'not a valid format for attribute [age]'
                               f' must be type int!, eg: (18, 19 40)')
                         return
             else:
@@ -265,43 +291,12 @@ class HBNBCommand(cmd.Cmd):
                 return
 
     def do_quit(self, arg) -> bool:
+        """
+            oijhoeiwfwe
+        """
         print("Thank you for having an account with us! 🚀")
         return True
 
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-
-
-
-
-# create State name kelly
-# yes it has the attribute
-# Please Keep your id private if lost, can't access account (eb870e25-5899-428a-a842-2d3cea8c1f3f)
-
-# create <Class name> <param 1> <param 2> <param 3>...
-
-"""
-def do_create(self, arg) -> None:
-        regex = re.compile(pattern)
-        matches = regex.search(arg)
-
-        if matches:
-            print(matches.groups())
-        #     matchgroup = matches.groups()
-        #     if matchgroup[3].startswith("\"") and matchgroup[3].endswith("\"") and ' ' in matchgroup[3]:
-        #         modified_string = matchgroup[3].replace(" ", "_").strip('"')
-        #         print(modified_string)
-        #
-        #         if matchgroup[0] in self.ALL_CLASSES:
-        #             obj = self.ALL_CLASSES[matchgroup[0]]()
-        #             setattr(obj, matchgroup[2], modified_string)
-        #             print(obj)
-        #         else:
-        #             print('we no see am')
-        #     else:
-        #         print('noting come out')
-        else:
-            print('noting come out')
-
-"""
